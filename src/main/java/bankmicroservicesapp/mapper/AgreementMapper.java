6601681b4ab0c8761f0538c42708fc155b7e761a
Vladislav <tokenvlad@gmail.com>
2023-09-28 22:01:38 +0200

    update mapper + query

--- src/main/java/bankmicroservicesapp/mapper/AgreementMapper.java
+++ src/main/java/bankmicroservicesapp/mapper/AgreementMapper.java
@@ -1,22 +1,6 @@
 package bankmicroservicesapp.mapper;
 
-import bankmicroservicesapp.entity.Agreement;
-import bankmicroservicesapp.entity.enums.StatusProduct;
-import bankmicroservicesapp.entity.enums.TypeProduct;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.mapstruct.Mapper;
-
-import java.time.LocalDateTime;
-import java.util.Objects;
-import java.util.Set;
-import java.util.UUID;
-
-import static jakarta.persistence.CascadeType.*;
 
 @Mapper(componentModel = "spring")
 public interface AgreementMapper {
